6d4f14fa9d42fa33f767b5d3ec7d9c5de82f58f6
Jonathan <j.moya.jt@gmail.com>
2022-02-01 08:43:12 -0600

    create base page for shopping history

---
+++ lib/modules/groceries/pages/shopping_history_page.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class GroceriesShoppingHistoryPage extends StatelessWidget {
+  const GroceriesShoppingHistoryPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: const CupertinoNavigationBar(
+          middle: Text('Historial de compras'),
+        ),
+        body: ListView.builder(
+            itemCount: 3,
+            itemBuilder: (_, int index) {
+              return ListTile(
+                title: Text('0${index + 1} diciembre 2021'),
+                subtitle: const Text('Soriana'),
+                trailing: const Icon(Icons.keyboard_arrow_right),
+                onTap: () {},
+              );
+            }));
+  }
+}
