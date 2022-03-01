17ed71370393e39dbb999f5b001be1692e9bd927
Jonathan <j.moya.jt@gmail.com>
2022-02-28 18:34:45 -0600

    AB#51 create store for payment

---
+++ lib/modules/groceries/stores/payment_store.dart
@@ -0,0 +1,46 @@
+import 'package:mobx/mobx.dart';
+import 'package:money_manager/modules/groceries/stores/receipt_history_store.dart';
+import 'package:money_manager/modules/groceries/stores/shopping_cart_store.dart';
+
+import 'cart_item_store.dart';
+import 'product_store.dart';
+
+part 'payment_store.g.dart';
+
+class PaymentStore = _PaymentStore with _$PaymentStore;
+
+abstract class _PaymentStore with Store {
+  _PaymentStore(
+      {required this.receiptHistoryStore,
+      required this.shoppingCartStore,
+      required this.productListStore});
+
+  late ShoppingCartStore shoppingCartStore;
+
+  late ReceiptHistoryStore receiptHistoryStore;
+
+  late ProductListStore productListStore;
+
+  @action
+  void createReceipt() {
+    shoppingCartStore.setId();
+
+    receiptHistoryStore.saveReceipt(
+        shoppingCartStore.id!,
+        shoppingCartStore.storeName,
+        shoppingCartStore.buyDate,
+        shoppingCartStore.checkedItems);
+
+    updatePriceItem(shoppingCartStore.checkedItems);
+
+    // Clear cart
+    shoppingCartStore.cleanCart();
+  }
+
+  @action
+  void updatePriceItem(List<CartItemStore> checkedItems) {
+    for (var item in checkedItems) {
+      productListStore.edit(item.product);
+    }
+  }
+}
---
+++ lib/modules/groceries/stores/payment_store.g.dart
@@ -0,0 +1,43 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'payment_store.dart';
+
+// **************************************************************************
+// StoreGenerator
+// **************************************************************************
+
+// ignore_for_file: non_constant_identifier_names, unnecessary_brace_in_string_interps, unnecessary_lambdas, prefer_expression_function_bodies, lines_longer_than_80_chars, avoid_as, avoid_annotating_with_dynamic
+
+mixin _$PaymentStore on _PaymentStore, Store {
+  final _$_PaymentStoreActionController =
+      ActionController(name: '_PaymentStore');
+
+  @override
+  void createReceipt() {
+    final _$actionInfo = _$_PaymentStoreActionController.startAction(
+        name: '_PaymentStore.createReceipt');
+    try {
+      return super.createReceipt();
+    } finally {
+      _$_PaymentStoreActionController.endAction(_$actionInfo);
+    }
+  }
+
+  @override
+  void updatePriceItem(List<CartItemStore> checkedItems) {
+    final _$actionInfo = _$_PaymentStoreActionController.startAction(
+        name: '_PaymentStore.updatePriceItem');
+    try {
+      return super.updatePriceItem(checkedItems);
+    } finally {
+      _$_PaymentStoreActionController.endAction(_$actionInfo);
+    }
+  }
+
+  @override
+  String toString() {
+    return '''
+
+    ''';
+  }
+}
